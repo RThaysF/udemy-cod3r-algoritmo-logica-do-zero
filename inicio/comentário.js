a646b7361224800da15b8b0ca895c8868e3ffdd5
Thays Rachadel Ferreira <thaysrachadel@gmail.com>
2022-06-27 18:49:13 -0300

    projetinhos da semana

---
+++ inicio/comentário.js
@@ -0,0 +1,11 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log("Passo #01"); 
+console.log("Passo #02"); // mais um exemplo de console.log!
+// console.log("Passo #03"); linha foi comentada, então não vai ser executada
+
+/*
+esse é um 
+comentário de 
+multiplas linhas!
+*/
+console.log("Passo #04");
